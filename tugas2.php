4ec8e85a6caf27cf196d4d2cdcc8ac5a7e34eaa5
Decky Fajar Sidiq <46802206+deckyfajars@users.noreply.github.com>
2021-07-06 17:22:32 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,24 @@
+<?php
+  $hari1 = 36;
+  $pekerja1 = 12;
+  $pekerja2 = 24;
+  $hasil = $hari1 / 2;
+
+  echo "
+    $hari1 Hari = $pekerja1 Pekerja </br>
+    $hasil hari = 24 Pekerja </br>
+    X = <b>$hasil</b> hari?
+  ";
+
+  $jual = 80000;
+  $beli = 50000;
+  $hasil = (($jual-$beli)/$beli)*100;
+
+  echo "
+    <br>===============<br>
+    Jual = $jual </br>
+    Beli = $beli </br>
+    Keuntungan ? </br>
+    Jawaban : <b>$hasil %</b>
+  ";
+?>
